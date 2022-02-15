3c658f9870c2d895649861761c1ba8757897f1da
Tom Eloe <tomeloe@gmail.com>
2022-02-14 20:14:38 -0800

    add images/products.js/homescreen component

---
+++ mern-ecommerce-app/frontend/src/screens/HomeScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Row, Col } from 'react-bootstrap';
+import products from "../products";
+
+const HomeScreen = () => {
+    return ( 
+        <>
+            <h1>Latest Products</h1>
+        </>
+    )
+}
+
+export default HomeScreen
