c8822debc5928f68a0278c5e96cb6345f8549b83
Tom Eloe <tomeloe@gmail.com>
2022-02-21 15:17:04 -0800

    product details reducer & action

--- mern-ecommerce-app/frontend/src/screens/ProductScreen.js
+++ mern-ecommerce-app/frontend/src/screens/ProductScreen.js
@@ -1,26 +1,29 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { Link } from "react-router-dom";
-import { Row, Col, Image, ListGroup, Card, Button, ListGroupItem } from "react-bootstrap";
+import { useDispatch, useSelector } from 'react-redux';
+import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
 import Rating from '../components/Rating';
-import axios from 'axios';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
+import { listProductDetails } from '../actions/productActions';
 
 const ProductScreen = ({ match }) => {
 
-    const [product, setProduct] = useState({});
+    const dispatch = useDispatch();
+
+    const productDetails = useSelector(state => state.productDetails);
+    const { loading, error, product } = productDetails;
 
     useEffect(() => {
-        const fetchProduct = async () => {
-            const { data } = await axios.get(`/api/products/${match.params.id}`);
-            
-            setProduct(data);
-        }
-
-        fetchProduct();
-    }, [match])
+        dispatch(listProductDetails(match.params.id));
+    }, [dispatch, match])
 
     return (
         <>
-            <Link className='btn btn-light my-3' to='/'>Go Back</Link>
+            <Link className='btn btn-light my-3' to='/'>
+                Go Back
+            </Link>
+            {loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : 
             <Row>
                 <Col md={6}>
                     <Image src={product.image} alt={product.name} fluid />
@@ -64,7 +67,7 @@ const ProductScreen = ({ match }) => {
                                     </Col>
                                 </Row>
                             </ListGroup.Item>
-                            <ListGroup.Item>
+                            <ListGroup.Item className='d-grid gap-2'>
                                 <Button className='btn-block' type='button' disabled={product.countInStock === 0}>
                                     Add To Cart
                                 </Button>
@@ -73,6 +76,7 @@ const ProductScreen = ({ match }) => {
                     </Card>
                 </Col>
             </Row>
+            }
         </>
     )
 }
