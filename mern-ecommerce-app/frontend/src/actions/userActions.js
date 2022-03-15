0ce6f54c4562833ea8f1bf2d1d9f6804c197caaa
Tom Eloe <tomeloe@gmail.com>
2022-03-15 07:42:47 -0700

    user details & orders reset

--- mern-ecommerce-app/frontend/src/actions/userActions.js
+++ mern-ecommerce-app/frontend/src/actions/userActions.js
@@ -3,6 +3,7 @@ import {
     USER_DETAILS_FAIL,
     USER_DETAILS_REQUEST,
     USER_DETAILS_SUCCESS,
+    USER_DETAILS_RESET,
     USER_LOGIN_REQUEST, 
     USER_LOGIN_SUCCESS, 
     USER_LOGIN_FAIL, USER_LOGOUT, 
@@ -13,6 +14,7 @@ import {
     USER_UPDATE_PROFILE_REQUEST,
     USER_UPDATE_PROFILE_SUCCESS,
 } from "../constants/userConstants"
+import { ORDER_LIST_MY_RESET } from '../constants/orderConstants'
 
 export const login = (email, password) => async (dispatch) => {
     try {
@@ -43,8 +45,10 @@ export const login = (email, password) => async (dispatch) => {
 }
 
 export const logout = () => (dispatch) => {
-    localStorage.removeItem('userInfo');
-    dispatch({ type: USER_LOGOUT });
+    localStorage.removeItem('userInfo')
+    dispatch({ type: USER_LOGOUT })
+    dispatch({ type: USER_DETAILS_RESET })
+    dispatch({ type: ORDER_LIST_MY_RESET })
 }
 
 export const register = (name, email, password) => async (dispatch) => {
