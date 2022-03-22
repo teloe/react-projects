513677eb836f8f94491091e98cc40f7b98228254
Tom Eloe <tomeloe@gmail.com>
2022-03-21 20:51:49 -0700

    admin user delete

--- mern-ecommerce-app/backend/routes/userRoutes.js
+++ mern-ecommerce-app/backend/routes/userRoutes.js
@@ -6,11 +6,16 @@ import {
     getUserProfile,
     updateUserProfile,
     getUsers,
+    deleteUser,
 } from '../controllers/userController.js'
 import {protect, admin} from '../middleware/authMiddleware.js';
 
 router.route('/').post(registerUser).get(protect, admin, getUsers)
 router.post('/login', authUser);
-router.route('/profile').get(protect, getUserProfile).get(protect, updateUserProfile).put(protect, updateUserProfile);
+router.route('/profile')
+    .get(protect, getUserProfile)
+    .get(protect, updateUserProfile)
+    .put(protect, updateUserProfile);
+router.route('/:id').delete(protect, admin, deleteUser);
 
 export default router 
