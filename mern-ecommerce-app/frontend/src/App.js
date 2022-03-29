ca7599df3e3e3669cc4bb6128d70ec02d20c4c11
Tom Eloe <tomeloe@gmail.com>
2022-03-29 16:44:15 -0700

    product pagination

--- mern-ecommerce-app/frontend/src/App.js
+++ mern-ecommerce-app/frontend/src/App.js
@@ -35,11 +35,18 @@ const App = () => {
                         <Route path='/product/:id' component={ProductScreen} />
                         <Route path='/admin/userlist' component={UserListScreen} />
                         <Route path='/admin/user/:id/edit' component={UserEditScreen} />
-                        <Route path='/admin/productlist' component={ProductListScreen} />
+
+                        <Route path ='/admin/productlist' component={ProductListScreen} exact />
+                        <Route path ='/admin/productlist/:pageNumber' component={ProductListScreen} exact />
+
                         <Route path='/admin/product/:id/edit' component={ProductEditScreen} />
                         <Route path='/admin/orderlist' component={OrderListScreen} />
                         <Route path='/cart/:id?' component={CartScreen} />
-                        <Route path='/search/:keyword' component={HomeScreen} />
+
+                        <Route path='/search/:keyword' component={HomeScreen} exact />
+                        <Route path='/page/:pageNumber' component={HomeScreen} exact />
+                        <Route path='/search/:keyword/page/:pageNumber' component={HomeScreen} exact />
+
                         <Route path='/' component={HomeScreen} exact />
                     </Container>
                 </main>
