d6eaa419bd49d06ca030974428f197bc47597dec
Tom Eloe <tomeloe@gmail.com>
2022-04-18 21:36:10 -0700

    initial state

--- calculator/src/components/App.js
+++ calculator/src/components/App.js
@@ -3,6 +3,15 @@ import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            total: null,
+            next: null,
+            operation: null
+        };
+    }
+
     handleClick = (buttonName) => {
         console.log('click', buttonName);
     };
@@ -10,7 +19,7 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <Display value="117" />
+                <Display value={this.state.next || this.state.total || '0'} />
                 <ButtonPanel clickHandler={this.handleClick} />
             </div>
         );
